47e1ebfbc403c6d3d61d1f9a05b5e558d3abae0c
Udbhav Gupta <gupta.udbhav@gmail.com>
2014-05-15 11:51:49 -0400

    upgrade ruby build when provisioning

--- vendor/cookbooks/ghalib/attributes/default.rb
+++ vendor/cookbooks/ghalib/attributes/default.rb
@@ -1,6 +1,7 @@
 default['ghalib']['user'] = 'vagrant'
 default['ghalib']['app_root'] = '/vagrant'
 default['ghalib']['home'] = '/home/' + default['ghalib']['user']
+default['ruby_build']['upgrade'] = 'yes'
 
 default['rbenv']['user_installs'] = [{
     user: 'vagrant',
